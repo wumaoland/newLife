35376aa599c1e55cabdd95048d520f77db72b2f3
张志强 <398560549@qq.com>
2020-06-29 20:37:38 +0800

    update

---
+++ js相关/考题/比较绕的题.js
@@ -0,0 +1,30 @@
+let a;
+//需要怎样做 
+//1、
+a  = {
+    b:1,
+    toString : function(){
+        return this.b++;
+    },
+    valueOf:function(){
+        return this.b++
+    }//两者均可
+}
+
+//2、
+a = [1,2,3]
+a.toString = a.shift
+a.join = a.shift
+//两种均可
+
+//3、
+var i = 0;
+Object.defineProperty(window,'a',{// 若使用此方法则不能定义a
+    get(){
+        return ++i;
+    }
+})
+
+if(a == 1 && a == 2 && a == 3){
+    console.log('做对了')
+}
