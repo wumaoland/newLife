60ad18f24c2100c1486d1c9a0bd0f17dd7143784
张志强 <398560549@qq.com>
2020-07-02 22:31:57 +0800

    update

--- js相关/手写方法/练手.js
+++ js相关/手写方法/练手.js
@@ -109,7 +109,7 @@ Function.prorotype._apply = function(context){
     context[key](args)
     delete context[key]
 }
-Function.prototype._bind = function(){
+Function.prototype._bind = function(context){
     let me = this
     arguments = Array.from(arguments)
     let context = arguments.shift()
@@ -124,4 +124,161 @@ function create(){
     let res = constructor.apply(obj,arguments)
 
     return typeof res === 'object' ? obj : {}
+}
+
+/**
+ * 2020-07-01 22:12:58
+ * @param {*} fn 
+ * @param {*} wait 
+ */
+function debounce(fn,wait){
+    let timer;
+    return function(){
+        let context = this;
+        let args = arguments
+        clearTimeout(timer)
+        timer = setTimeout(function(){
+            fn.apply(context,args)
+        },wait)
+    }
+}
+
+function throttle(fn,wait){
+    let timer = 0;
+    return function(){
+        let context = this;
+        let args = arguments;
+        if(Date.now() - timer > wait){
+            timer = Date.now()
+            fn.apply(context,args)
+        }
+    }
+}
+
+function filtten(arr){
+    return arr.reduce((result,item)=>{
+        return reduce.concat(Array.isArray(item) ? filtten(arr) : item)
+    },[])
+}
+
+Function.prorotype._call = function(context){
+    let args = Array.from(arguments.slice(1))
+    let key = Date.now().toString(32).slice(0,8)
+    context[key] = this
+    let res = context[key](...args)
+    delete context[key]
+    return res
+}
+
+Function.prototype._apply = function(context){
+    arguments = Array.from(arguments)
+    let context = arguments.shift()
+    let key = Date.now().toString(32).slice(0,8)
+    context[key] = this
+    let res = context[key](arguments)
+    delete(context)
+    return res
+}
+
+Function.prototype._bind = function(context){
+    let me = this
+    let args = Array.from(arguments).slice(1)
+    return function(){
+        return me.apply(context,Array.from(arguments).concat(args))
+    }
+}
+
+Function.prorotype._call = function(context){
+    context = context || window
+    let args = Array.fron(arguments).slice(1)
+    let key = Date.now().toString(32).slice(0,8)
+    context[key] = this
+    let res = context[key](...args)
+    delete context[key]
+    return res
+}
+
+Function.prototype._apply = function(context){
+    context = context || window
+    let args = Array.fron(arguments).slice(1)
+    let key = Date.now().toString(32).slice(0,8)
+    context[key] = this
+    let res = context[key](args)
+    delete context[key]
+    return res
+}
+
+Function.prototype._bind = function(context){
+    let me = this
+    let args = Array.fron(arguments).slice(1)
+    return function F(){
+        if(this instanceof F){
+            return this.apply(arguments.concat(args))
+        }
+        return context.apply(me,arguments.concat(args))
+    }
+}
+
+function _instanceOf(left,right){
+    left = left.__proto__
+    right = right.prototype
+    while(left){
+        if(left === right)return true;
+        if(left.__proto__ === null)return false;
+        left = left.__proto__
+    }
+}
+
+/**
+ * 2020-07-02 22:21:52
+ */
+
+function debounce(fn,wait){
+    let timer;
+    return function(){
+        let context = this;
+        let args = Array.from(arguments);
+        clearTimeout(timer)
+        timer = setTimeout(function(){
+            fn.apply(context,args)
+        },wait)
+    }
+}
+
+function throttle(fn,wait){
+    let timer = 0;
+    return function(){
+        let context = this;
+        let args = Array.from(arguments)
+        if(Date.now() - timer > wait){
+            timer = Date.now()
+            fn.apply(context,args)
+        }
+    }
+}
+
+function filtten(arr){
+    return arr.reduce(function(result,item){
+        return result.concat(Array.isArray(item) ? filtten(item) : item)
+    },[])
+}
+
+function _instanceOf(left,right){
+    left = left.__proto__
+    right = right.prototype
+    while(true){
+        if(left === null)return false;
+        if(left === right)return true;
+        left = left.__proro__
+    }
+}
+
+Function.protrotype._call = function(context){
+    context = context || window
+    let args = Array.fron(arguments).slice(1)
+    let key = Date.now().toString(32).slice(0,8)
+    context[key] = this
+    let res = context[key](...args)
+    delete context[key]
+    return res
 }
