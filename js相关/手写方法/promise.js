648c83697b16254fa5063855f6382caa3d44c3f2
zhangzhiqian43 <zhangzhiqian6>
2020-07-11 17:45:29 +0800

    update

--- js相关/手写方法/promise.js
+++ js相关/手写方法/promise.js
@@ -16,3 +16,49 @@ Promise._all = function(promises){
 }
 
 
+function Promise(fn){
+  let me = this;
+  let status = 'pending';
+  let value = undefined;
+  let reason = undefined;
+
+  function resolve(val){
+    if(status === 'pending'){
+      status = 'fulfilled'
+      value = val
+    }
+  }
+
+  function reject(err){
+    if(status === 'pending'){
+      status = 'rejected'
+      reason = err
+    }
+  }
+
+  try{
+    fn(resolve,reject)
+  }catch(err){
+    reject(err)
+  }
+
+  Promise.prototype.then = function(resolve,reject){
+    let self = this;
+    if(self.state === 'fulfilled'){
+      resolve(self.value)
+    }
+    if(self.state === 'rejected'){
+      reject(self.reason)
+    }
+  }
+}
+
+let promise = new Promise((resolve,reject)=>{
+  resolve('return resolve')
+})
+
+promise.then(data=>{
+  console.log('success',data)
+},err=>{
+  console.log('err',err)
+})
